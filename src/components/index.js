3ab4e0dc4ce3dd72d752ad2bd40651c1a76e01c5
Benjamin-25 <pabagonzalezbenjamin@gmail.com>
2026-01-27 23:02:53 -0500

    Implements some custom components to see effects

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import TextGenerateEffect from "@/components/TextGenerateEffect";
+import BoxShadow from "@/components/BoxShadow";
+import { TypewriterEffectSmooth } from "@/components/TypeWriter";
+
+export { TextGenerateEffect, BoxShadow, TypewriterEffectSmooth };
---
+++ src/constants/messages.constants.js
@@ -0,0 +1,4 @@
+export const MESSAGES = {
+  ABOUT_ME:
+    "Desarrollador Frontend por mas de 5 años, con amplios conocimientos en Desarrollo Web, mediante el uso de Stack Tecnológicos de vanguardia tales como React, JavaScript, TypeScript, CSS, SCSS, Bootstrap, Git, Mongo, Php, entre otros. Comprometido en aportar siempre su conocimiento en los proyectos a los que participo, ayudando a resolver los conflictos dentro del equipo de desarrollo, proactivo y siempre me mantengo en constante evolución frente a los retos que deparan en el mundo digital de hoy.",
+};
